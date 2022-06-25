a1d5d0e2ffe0009af183d01791035ea8e45d7e01
YiqianFan <82208828+YiqianFan@users.noreply.github.com>
2022-06-25 21:50:26 +0800

    Add files via upload

---
+++ 源.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+//int main()
+//{
+//	printf("Hello World!");
+//	return 0;
+//}
+
+//int main()
+//{
+//	char test = 'A';
+//	printf("%c\n\n", test);
+//
+//	int age = 21;
+//	printf("%d\n", age);
+//	return 0;
+//}
+
+int main()
+{
+	printf("Hello My Friend!\n\n");
+
+	char A = 'A';
+	printf("%c\n\n",A);
+
+	int height = 165;
+	printf("%d\n", height);
+
+	return 0;
+}
